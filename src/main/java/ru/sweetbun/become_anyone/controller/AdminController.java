cf1a6fd483451b97238785fbceb06c8f77dc97b9
Semyon <s.e.m.e.n.e.m.tancor.disco@gmail.com>
2024-11-13 03:15:15 +0700

    remove imports

--- src/main/java/ru/sweetbun/become_anyone/controller/AdminController.java
+++ src/main/java/ru/sweetbun/become_anyone/controller/AdminController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.sweetbun.become_anyone.entity.User;
 import ru.sweetbun.become_anyone.service.UserService;
-
-import java.util.List;
 
 import static java.util.Optional.ofNullable;
 import static org.springframework.http.ResponseEntity.ok;
