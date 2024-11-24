646f368fb591d2085d1b25a1381090fa78a52563
Semyon <s.e.m.e.n.e.m.tancor.disco@gmail.com>
2024-11-24 21:05:42 +0700

    Configure remaining controllers

--- rest/src/main/java/ru/sweetbun/becomeanyone/controller/RoleController.java
+++ rest/src/main/java/ru/sweetbun/becomeanyone/controller/RoleController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +21,14 @@ public class RoleController {
 
     private final RoleService roleService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/teacher")
     @Operation(summary = "Назначить пользователя учителем", description = "Добавляет новую роль пользователю")
     public ResponseEntity<?> appointTeacher(@PathVariable Long userId) {
         return ok(roleService.appointTeacher(userId));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/admin")
     @Operation(summary = "Назначить пользователя админом", description = "Добавляет новую роль пользователю")
     public ResponseEntity<?> appointAdmin(@PathVariable Long userId) {
